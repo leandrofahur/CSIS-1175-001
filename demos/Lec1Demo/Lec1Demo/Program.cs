3f5c43ab5da2d93035e4c3b8532d5b476e255428
leandrofahur <leandrofahur@gmail.com>
2020-09-08 11:56:39 -0700

    filter file

---
+++ demos/Lec1Demo/Lec1Demo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lec1Demo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
