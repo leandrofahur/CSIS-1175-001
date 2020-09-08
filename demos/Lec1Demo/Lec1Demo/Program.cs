631a4cb120a85b84e224f0adc746703b675b01ba
leandrofahur <leandrofahur@gmail.com>
2020-09-08 12:26:39 -0700

    my first c# project

--- demos/Lec1Demo/Lec1Demo/Program.cs
+++ demos/Lec1Demo/Lec1Demo/Program.cs
@@ -6,7 +6,17 @@ namespace Lec1Demo
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // The method Write on the terminal and doesn't skip the line.
+            Console.Write("Hello World! ");
+            // Write in the terminal and skip one line.
+            Console.WriteLine("My name is Leandro");
+            /*
+             * This
+             * is
+             * a
+             * multi line
+             * comment
+             */
         }
     }
 }
