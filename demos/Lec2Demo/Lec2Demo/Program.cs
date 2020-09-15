00eafaf732a381b3b6d0a63f86c9f36143e65fb6
leandrofahur <leandrofahur@gmail.com>
2020-09-15 11:30:54 -0700

    Lec2Demo exercise

--- demos/Lec2Demo/Lec2Demo/Program.cs
+++ demos/Lec2Demo/Lec2Demo/Program.cs
@@ -1,12 +1,28 @@
-﻿using System;
+﻿using System; // using directive is to existing classes and namespaces
+using static System.Console; // import the Console Class
 
+
+// The namespace is a collection of classes
 namespace Lec2Demo
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //System.Console.Write("Test...");
+            //Console.WriteLine("Hello World!"); // Calling WriteLine method
+            //WriteLine("This is line 2");
+            //WriteLine("This is line 3");
+            //WriteLine("********************************");
+            Console.WriteLine("**********************");
+            Console.WriteLine("*                    *");
+            Console.WriteLine("*    Name: Leandro   *");
+            Console.WriteLine("*    Dept: CSIS      *");
+            Console.WriteLine("*    Faculty: CBA    *");
+            Console.WriteLine("*                    *");
+            Console.WriteLine("**********************");
         }
     }
 }
+
+
