853a68e69843a9c18791674dbb68e7ed8effd831
leandrofahur <leandrofahur@gmail.com>
2020-09-15 12:21:53 -0700

    Lec2Demo ReadLine and display input

--- demos/Lec2Demo/Lec2Demo/Program.cs
+++ demos/Lec2Demo/Lec2Demo/Program.cs
@@ -14,13 +14,66 @@ namespace Lec2Demo
             //WriteLine("This is line 2");
             //WriteLine("This is line 3");
             //WriteLine("********************************");
+
+            //char c2c = 'l';
+            //Console.WriteLine("**********************");
+            //Console.WriteLine("*                    *");
+            //Console.WriteLine("*    Name: Leandro   *");
+            //Console.WriteLine("*    Dept: CSIS      *");
+            //Console.WriteLine("*    Faculty: CBA    *");
+            //Console.WriteLine("*                    *");
+            //Console.WriteLine("**********************");
+            //WriteLine(c2c);
+
+            //// This is a pascal case variable:
+            //int ThisIsAPascalCaseVariable = -60;
+            //WriteLine(ThisIsAPascalCaseVariable);
+
+            //// This is a camel case example:
+            //int thisIsCamelCase = 60;
+            //WriteLine(thisIsCamelCase);
+
+
+            //Console.WriteLine("**********************");
+            //Console.WriteLine("*                    *");
+            //Console.WriteLine("*    Name: Leandro   *");
+            //Console.WriteLine("*    Dept: CSIS      *");
+            //Console.WriteLine("*    Faculty: CBA    *");
+            //Console.WriteLine("*                    *");
+            //Console.WriteLine("**********************");
+
+            WriteLine(); // Insert blank line
+            WriteLine("Lec2: Demo 2 - getting user input and using string variables");
+            string userName = "";
+            string userDepartment = "";
+            string userFaculty = "";
+
+            Write("Please insert the user name: ");
+            userName = ReadLine();
+            WriteLine("Your name is " + userName);
+
+            WriteLine();
+            Write("Please insert the department: ");
+            userDepartment = ReadLine();
+            userDepartment = userDepartment.ToUpper();
+            WriteLine("Your department is " + userDepartment);
+
+            WriteLine();
+            Write("Please insert the faculty: ");
+            userFaculty = ReadLine();
+            userFaculty = userFaculty.ToUpper();
+            WriteLine("Your faculty is "  + userFaculty);
+
+            WriteLine();
             Console.WriteLine("**********************");
             Console.WriteLine("*                    *");
-            Console.WriteLine("*    Name: Leandro   *");
-            Console.WriteLine("*    Dept: CSIS      *");
-            Console.WriteLine("*    Faculty: CBA    *");
+            Console.WriteLine("*    Name: " + userName + "   *");
+            Console.WriteLine("*    Dept: " + userDepartment + "      *");
+            Console.WriteLine("*    Faculty: " + userFaculty + "    *");
             Console.WriteLine("*                    *");
             Console.WriteLine("**********************");
+
+
         }
     }
 }
