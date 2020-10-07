3df5174affd045d5941bf79eb491008893972b21
leandrofahur <leandrofahur@gmail.com>
2020-10-06 19:36:27 -0700

    exercise finished

--- demos/Lec5Demos/RealEstInvest/RealEstInvest/Program.cs
+++ demos/Lec5Demos/RealEstInvest/RealEstInvest/Program.cs
@@ -17,21 +17,31 @@ namespace RealEstInvest
 
             WriteLine("The details of the real estate you entered are...");
             WriteLine(myRealEstate); //note you say WriteLine(object/instance); automatically calls ToString()
-            
+
             //Call the GetIncomeAndExpense method with out arguments for income and rent
             //Set the IncomeFromRent and MonthlyExpense properties of myRealEstate object
             //WriteLine the object to see if the user values are displayed right
-
+            GetIncomeAndExpense(out double incomeA, out double expenseA);
+            WriteLine("The details of the real income and expenses you entered...");
+            myRealEstate.IncomeFromRent = incomeA;
+            myRealEstate.MonthlyExpense = expenseA;
+            //WriteLine("{0}", myRealEstate.IncomeFromRent);
+            //WriteLine("{0}", myRealEstate.MonthlyExpense);
+            WriteLine(myRealEstate);
             //Write 5 parameter version of the constructor in the class - address, year, price, income, expense
             //Write a method in the Main program that gets user input for all these returns a string corresponding 
             //to the address, and sets out parameters for year, price, income and expense
+
             //Call this method to get all values from user and creates object using the 5-parameter constructor
+            GetAllParameters(out string stAddress, out int yearBuilt, out double price, out double income, out double expense);
+            RealEstate rE = new RealEstate(yearBuilt, stAddress, price, income, expense);
+            Console.WriteLine(rE);
         }
 
         static void GetPropertyInfo(out string stAddress, out int yearBuilt, out double price)
         {
             Clear(); //clearing console window
-            WriteLine("Enter the real estate information here...");
+            WriteLine("Enter the real state information here...");
             Write("Enter the street address: ");
             stAddress = ReadLine(); //gets street address from user and assigns it to the out parameter
             Write("Enter the year built: ");
@@ -42,6 +52,11 @@ namespace RealEstInvest
         static void GetIncomeAndExpense(out double incomeFromRent, out double monthlyExpense)
         {
             //get income and expense from user after giving appropriate Write messages to the user
+            //Clear();
+            Write("Enter the income: ");
+            incomeFromRent = double.Parse(ReadLine());
+            Write("Enter the expense: ");
+            monthlyExpense = double.Parse(ReadLine());
             //set the value of the out parameters
             //Note that syntax error is because out parameters are not set
         }
@@ -50,6 +65,20 @@ namespace RealEstInvest
         //and has four out parameters set for 
         //year, purchase price, income, expense
         //all input comes from the user
+        static void GetAllParameters(out string stAddress, out int yearBuilt, out double price, out double income, out double expense)
+        {
+            WriteLine("Enter the real state information here...");
+            Write("Enter the street address: ");
+            stAddress = ReadLine(); //gets street address from user and assigns it to the out parameter
+            Write("Enter the year built: ");
+            yearBuilt = int.Parse(ReadLine());
+            Write("Enter the purchase price: ");
+            price = double.Parse(ReadLine());
+            Write("Enter the income: ");
+            income = double.Parse(ReadLine());
+            Write("Enter the expense: ");
+            expense = double.Parse(ReadLine());
+        }
 
     }
 }
